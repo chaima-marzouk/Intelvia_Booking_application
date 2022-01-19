e1a6a0ba05c64e2f7b8346f31acfe14b826f5607
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-01-19 03:09:23 +0100

    create champs in user schema

--- models/user.js
+++ models/user.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
+    _id: Schema.Types.ObjectId,
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
 
