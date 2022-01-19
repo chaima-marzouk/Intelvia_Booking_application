3e1bb29497f7e3be8a1eb8a74a5458d809bbbb4d
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-01-19 11:41:41 +0100

    user registration

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
-// const router = express.Router();
-const Users = require('../controller/users')
+const mongoose = require('mongoose')
+const Users = require('../models/user')
 
 router.get('/test', (req,res) => {
     res.send("test")
+})
+
+router.post('/register', async(req,res) =>{
+    const user = new Users({
+        _id: mongoose.Types.ObjectId(),
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password  
+    })
+    const savedUser = await user.save();
+    try{
+        res.status(200).send({message: 'USER HAS BEEN REGESTRED :) !'})
+    } catch(error){
+        res.status(500).send(error)
+    }
 })
 
 
