aef0b37d8a137cda7d4d5c56a705fe807a94cfcb
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-01-26 15:12:13 +0100

    create router for login

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const userController = require('../controller/users.controller')
 
 
-router.post('/register', userController.registeration)
+router.post('/register', userController.registeration);
+router.post('/login', userController.login);
 
 
 module.exports = router
