19c6bf07f556b1483b2e86ca96460f700c520b72
chaima marzouk <marzouk.chaima.892@gmail.com>
2022-01-26 15:10:06 +0100

    create controller to separate modules

---
+++ controller/users.js
@@ -0,0 +1,35 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const { registerValidation } = require('../validation');
+
+
+exports.registeration =  async (req,res) =>{
+
+    //Data Validation 
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const existingUser = await User.findOne({email:req.body.email});
+
+    //check if email exist
+    if(existingUser) return res.status(400).send({message: 'EMAIL ALREADY EXIST'})
+
+    // hash password 
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = bcrypt.hashSync(req.body.password, salt);
+    
+    try{
+        const user = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashPassword
+        })
+
+        res.status(201).json({ status: "success", user: user});
+    } catch(error){
+        res.status(404).json({
+            status: "error",
+            message: error
+        });
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,40 +1,8 @@
 const router = require('express').Router();
-const app = require('express');
-const User = require('../models/user')
-const { registerValidation } = require('../validation');
-const bcrypt = require('bcryptjs')
+const userController = require('../controller/users')
 
 
-router.post('/register', async (req,res) =>{
-
-    //Data Validation 
-    const {error} = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const existingUser = await User.findOne({email:req.body.email});
-
-    //check if email exist
-    if(existingUser) return res.status(400).send({message: 'EMAIL ALREADY EXIST'})
-
-    // hash password 
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = bcrypt.hashSync(req.body.password, salt);
-    
-    try{
-        const user = await User.create({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashPassword
-        })
-
-        res.status(201).json({ status: "success", user: user});
-    } catch(error){
-        res.status(404).json({
-            status: "error",
-            message: error
-        });
-    }
-})
+router.post('/register', userController.registeration)
 
 
 module.exports = router
